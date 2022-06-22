cec2fa47d521da29318234c33e764f6b56866c1e
Abdullah Ahmed <74293921+abdullahgt@users.noreply.github.com>
2022-06-22 14:40:17 -0400

    Time: 53 ms (28.19%), Space: 13.9 MB (15.47%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -4,7 +4,6 @@ class Solution:
             return -1
         if haystack == needle:
             return 0
-        # Length of the needle
         needleLength = len(needle)
         # Loop through the haystack and slide the window
         for i in range(len(haystack) - needleLength + 1):
